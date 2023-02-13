38263aa963ddb027a98f3a346616850f9ea33cd2
tyrellondijo00 <95715931+tyrellondijo00@users.noreply.github.com>
2023-02-13 23:33:03 +0300

    App Complete

--- Face_Detector.py
+++ Face_Detector.py
@@ -2,9 +2,6 @@ import cv2
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #Load some pretrained data on face frontals from opencv (haar cascade algorithm)
 
-#Choose Image To Detect Faces in. imRead means image read
-#img = cv2.imread('RDJ.jpg') 
-
 #Capture Video from webcam
 webcam = cv2.VideoCapture(0)
 
@@ -13,23 +10,22 @@ while True:
     #Read the current frame
     successful_frame_read, frame = webcam.read()
 
-grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Make the image grayscale for the algorithm
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Make the image grayscale for the algorithm
 
-cv2.imshow('Clever Programmer Face Detector', grayscaled_img) #Clever Programmer Face Detector is the name of the window that pops up
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img) #Detect the faces
 
-cv2.waitKey(1)#Makes the program to not instatntly close
+    #Draw squares on faces
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 2)
 
-"""
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img) #Detect the faces
+    cv2.imshow('Tyrells Face Detector', frame) #Clever Programmer Face Detector is the name of the window that pops up
 
-#Draw squares on faces
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0,255,0), 2)
+    key = cv2.waitKey(1) #Makes the program to not instatntly close
 
+    #Stop if q is pressed
+    if key ==81 or key==113:
+        break
 
-cv2.imshow('Clever Programmer Face Detector', img) #Clever Programmer Face Detector is the name of the window that pops up
-
-cv2.waitKey()#Makes the program to not instatntly close
-"""
+webcam.release()
 
 print("Code Completed")
